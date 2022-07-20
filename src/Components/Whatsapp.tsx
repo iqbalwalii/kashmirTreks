d48da54d91b5b7beeb910de304cdf321271ba108
iqbalwalii <iqbalwalii@tuta.io>
2022-07-20 21:40:45 +0530

    homepage completed

--- src/Components/Whatsapp.tsx
+++ src/Components/Whatsapp.tsx
@@ -9,7 +9,7 @@ const Whatsapp = () => {
         target="_blank"
       >
         <Image
-          src={`/assets/images/whatsapp.svg`}
+          src="https://res.cloudinary.com/kashmir-ensueno1/image/upload/v1658333406/WhatsApp_ls56pn.svg"
           width={65}
           height={65}
           alt="whatsapp"
