956acf245439835e487970b35fe121fce8c234b0
iqbalwalii <iqbalwalii@tuta.io>
2022-08-21 16:09:22 +0530

    changed trek

--- src/data/sunday.ts
+++ src/data/sunday.ts
@@ -1,14 +1,14 @@
 const sunday = {
   title: "Sunday Trek",
-  name: "Vishansar Kishansar Lake",
+  name: "Kounsarnag Lake Trek",
   type: "meadow", // "trek", "meadow", "valley", "Alpine Lake", "camp", "hike", "nature", "wildlife", "waterfall", "scenic", "other"
   description: "Bookings Open",
   drive: "4 hours (ROund Trip)",
   distance: "2.5 km",
   image1:
-    "https://res.cloudinary.com/kashmir-ensueno1/image/upload/v1658321507/gang_oncssl.jpg",
+    "https://res.cloudinary.com/kashmir-ensueno1/image/upload/v1658329148/249aab66375ed89d6cba7_hohv2k.jpg",
   image2:
-    "https://res.cloudinary.com/kashmir-ensueno1/image/upload/v1658321507/gang_oncssl.jpg",
+    "https://res.cloudinary.com/kashmir-ensueno1/image/upload/v1658329148/249aab66375ed89d6cba7_hohv2k.jpg",
 };
 
 export default sunday;
