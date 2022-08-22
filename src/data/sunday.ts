2dad96ce7d0ac97ed1b533fb22ad9a585b838203
kashmirensueno <kashmirensueno@gmail.com>
2022-08-22 08:42:34 +0530

    new lake

--- src/data/sunday.ts
+++ src/data/sunday.ts
@@ -1,9 +1,9 @@
 const sunday = {
   title: "Sunday Trek",
-  name: "Bihari Lake trek",
+  name: "New Bengali Trek",
   type: "meadow", // "trek", "meadow", "valley", "Alpine Lake", "camp", "hike", "nature", "wildlife", "waterfall", "scenic", "other"
   description: "Bookings Open",
-  drive: "4 hours (ROund Trip)",
+  drive: "4 hours (Round Trip)",
   distance: "2.5 km",
   image1:
     "https://res.cloudinary.com/kashmir-ensueno1/image/upload/v1658329148/249aab66375ed89d6cba7_hohv2k.jpg",
