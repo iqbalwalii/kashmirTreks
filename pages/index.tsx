8ba9ad920c1046bf25089afc3bf87d8b8a307430
iqbalwalii <iqbalwali786@gmail.com>
2022-11-24 15:44:18 +0530

    maintainance

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>KASHMIR ENSUENO</h1>
+      <h5>UNDER MAINTAINANCE</h5>
+    </div>
+  );
+}
